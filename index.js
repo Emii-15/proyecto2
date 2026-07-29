1d5e42c2aca039ec42d176c5a8c7452505f63b74
Emii-15 <emilianoledesma250@gmail.com>
2026-07-29 03:07:14 -0300

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-// Vercel solo despliega como función serverless lo que está dentro de /api.
-// Reexportamos el handler que ya exporta server.js, sin duplicar lógica.
-module.exports = require('../server.js');
